b676de9f9ff4876ac39f45058930f369d31502c5
19wh1a05c0 <69683128+19wh1a05c0@users.noreply.github.com>
2020-08-17 15:50:26 +0530

    Add files via upload

---
+++ week2/binarynon.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int binarysearch(int first, int last, int a[50], int key)
+{
+	int pos, mid;
+	if(first <= last)
+	{
+		mid = (first + last)/2;
+		if(a[mid] == key)
+			return mid;
+		else if(a[mid] < key)
+			binarysearch(mid + 1, last, a, key);
+		else
+			binarysearch(first, mid - 1, a, key);
+	}
+	return pos;
+}
+int main()
+{
+	int n, pos, key, i, a[100];
+	scanf("%d",&n);
+	for(i = 0; i < n; i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&key);
+	pos = binarysearch(0, n-1, a, key);
+	if(pos == -1)
+	printf("element not found\n");
+	else
+	printf("element found at %d location",pos+1);
+}
